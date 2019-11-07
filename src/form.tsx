9228dbc8ff73c8891c8f720dcf3cef8280124e59
Deric Cain <deric.cain@daxko.com>
2019-11-07 06:48:27 -0600

    Added submit function to Form component

--- src/form.tsx
+++ src/form.tsx
@@ -2,20 +2,39 @@ import React from 'react';
 import { Form as FormModel, IFormOptions, IFormSchema, IInitialValues } from 'tiny-mobx-form';
 import { FormContext } from './form-context';
 
+interface Submit {
+  (values: any, errors: string[]): void;
+}
+
 export interface FormProps {
   fields: IFormSchema[];
   initialValues?: IInitialValues;
   options?: IFormOptions;
   children: React.ReactNode;
+  submit: Submit;
 }
 
 export const Form: React.FC<FormProps> = ({
   fields,
   initialValues = {},
   options = {},
+  submit,
   children,
 }) => {
   const form = new FormModel(fields, initialValues, options);
   const value = { form };
-  return <FormContext.Provider value={value}>{children}</FormContext.Provider>;
+
+  async function onSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.persist();
+    if (form.errors) {
+      form.showErrors();
+    } else {
+      await submit(form.values, form.errors);
+    }
+  }
+  return (
+    <FormContext.Provider value={value}>
+      <form onSubmit={onSubmit}>{children}</form>
+    </FormContext.Provider>
+  );
 };
